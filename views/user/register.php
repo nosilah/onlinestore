3c03a65c99068f32cb2877bd310fbb22d043f1f0
Abdul <alnosila.abd@gmail.com>
2021-03-29 16:58:07 +0300

    add view register

--- views/user/register.php
+++ views/user/register.php
@@ -11,14 +11,14 @@
                     <div class="signup-form"><!--sign up form-->
                         <h2>Регистрация на сайте</h2>
                         <form action="#" method="post">
-                            <input type="text" name="name" placeholder="Имя" value="<?php echo $name; ?>"/>
-                            <input type="email" name="email" placeholder="E-mail" value="<?php echo $email; ?>"/>
-                            <input type="password" name="password" placeholder="Пароль" value="<?php echo $password; ?>"/>
+                            <input type="text" name="name" placeholder="Имя" value=""/>
+                            <input type="email" name="email" placeholder="E-mail" value=""/>
+                            <input type="password" name="password" placeholder="Пароль" value=""/>
                             <input type="submit" name="submit" class="btn btn-default" value="Регистрация" />
                         </form>
                     </div><!--/sign up form-->
                 
-                <?php endif; ?>
+                
                 <br/>
                 <br/>
             </div>
